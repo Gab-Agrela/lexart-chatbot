5c7ac933523de5cafadb6908ca756d6aa4308337
GabrielAgrela <gabriel.agrela@hotmail.com>
2023-07-17 17:14:45 -0300

    feat: Creating chatbot responses

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,165 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
+const Chatbot = () => {
+  const [messages, setMessages] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isStarted, setIsStarted] = useState(false);
+  const [, setUser] = useState("");
+  const messagesContainerRef = useRef(null);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  const scrollToBottom = () => {
+    if (messagesContainerRef.current) {
+      messagesContainerRef.current.scrollTop =
+        messagesContainerRef.current.scrollHeight;
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const loanHandleClick = (e) => {
+    const { className, id } = e.target;
+    if (className === "option") {
+      const message = {
+        a: `<span>If you want to start a new business venture, 
+        renovate your home or consolidate debt, we are here to help you with your loan. 
+        Click <a href="http://loan-more-information-url" target="_blank">here</a>
+         to see more informations.</span>
+        `,
+        b: `Our loan conditions offer flexibility and convenience. 
+        Borrow any amount between $1,000 and $100,000 at competitive rates starting from 3.5%. 
+        Repay comfortably with flexible terms ranging from 6 months to 5 years and fixed monthly installments. 
+        Collateral may be required, and additional fees may apply. 
+        Review the lender's specific terms and conditions for accurate details.
+        Click <a href="http://loan-more-information-url" target="_blank">here</a>
+         to see more informations.</span>`,
+        c: `If you have any questions or need guidance regarding our loan application process, 
+        our friendly customer support team is here to help. 
+        Click <a href="http://loan-help-url" target="_blank">here</a>
+         to see more informations.</span>`,
+      };
+      const loanCustomMessage = {
+        type: "bot",
+        text: message[id],
+        dateTime: new Date().toISOString(),
+      };
+      return setMessages((prevState) => [...prevState, loanCustomMessage]);
+    }
+  };
+
+  const initialMessage = (messageText) => {
+    const initialMessageArray = ["Hello,", "Goodbye,", "Good,", "I want"];
+    if (initialMessageArray.some((str) => messageText.includes(str))) {
+      setIsStarted(true);
+      const firstMessage = {
+        type: "bot",
+        text: "Please, send your username and password. Example: username password",
+        dateTime: new Date().toISOString(),
+      };
+      return setMessages((prevState) => [...prevState, firstMessage]);
+    }
+  };
+
+  const authenticateUser = (inputValue) => {
+    if (inputValue.split(" ").length === 2) {
+      const [username, password] = inputValue.split(" ");
+      if (username && password) {
+        setIsAuthenticated(true);
+        setUser(username);
+
+        const welcomeMessage = {
+          type: "bot",
+          text: `Welcome, ${username}! Type "loan" to see the options.`,
+          dateTime: new Date().toISOString(),
+        };
+        setMessages((prevState) => [...prevState, welcomeMessage]);
+      }
+    } else {
+      const wrongFormat = {
+        type: "bot",
+        text: "Wrong format, send your username and password. Example: username password",
+        dateTime: new Date().toISOString(),
+      };
+      setMessages((prevState) => [...prevState, wrongFormat]);
+    }
+  };
+
+  const showLoanOptions = () => {
+    const mountText = `
+    <div class="loanOptions">
+      <span>Choose and click on one of the options below for more information:</span>
+      <span class="option" id="a">a - Do you want to apply for a loan?</span>
+      <span class="option" id="b">b - Loan conditions</span>
+      <span class="option" id="c">c - Help</span>
+    </div>`;
+    const loanMessage = {
+      type: "bot",
+      text: mountText,
+      dateTime: new Date().toISOString(),
+    };
+    return setMessages((prevState) => [...prevState, loanMessage]);
+  };
+
+  const handleMessageSubmit = (e) => {
+    e.preventDefault();
+    const messageText = e.target.message.value;
+    const newMessage = { type: "user", text: messageText };
+    setMessages([...messages, newMessage]);
+    if (!isAuthenticated && !isStarted) {
+      initialMessage(messageText);
+    }
+    if (!isAuthenticated && isStarted) {
+      authenticateUser(messageText);
+    }
+    if (messageText === "loan" && isAuthenticated) {
+      showLoanOptions();
+    }
+    setInputValue("");
+  };
+
+  return (
+    <ChatbotContainer>
+      <MessagesContainer ref={messagesContainerRef}>
+        {messages.map((message, index) => (
+          <Message key={index} isUser={message.type === "user"}>
+            {message.type === "user" ? (
+              <UserMessage dangerouslySetInnerHTML={{ __html: message.text }} />
+            ) : (
+              <BotMessage
+                dangerouslySetInnerHTML={{ __html: message.text }}
+                onClick={loanHandleClick}
+              />
+            )}
+          </Message>
+        ))}
+      </MessagesContainer>
+      <InputContainer onSubmit={handleMessageSubmit}>
+        <TextInput
+          type="text"
+          name="message"
+          placeholder="Type a message..."
+          value={inputValue}
+          onChange={handleInputChange}
+          autoComplete="off"
+        />
+        <SendButton type="submit" disabled={!inputValue}>
+          Send
+        </SendButton>
+      </InputContainer>
+    </ChatbotContainer>
+  );
+};
+
 const ChatbotContainer = styled.div`
-  width: 300px;
-  height: 400px;
+  width: 500px;
+  height: 600px;
   background-color: #fff;
   border-radius: 8px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
@@ -28,8 +184,10 @@ const UserMessage = styled.div`
   background-color: #2979ff;
   color: #fff;
   padding: 10px;
+
   border-radius: 20px;
   max-width: 70%;
+  word-wrap: break-word;
   font-family: "SF Pro Text", "Arial", sans-serif;
 `;
 
@@ -39,7 +197,24 @@ const BotMessage = styled.div`
   padding: 10px;
   border-radius: 20px;
   max-width: 70%;
+  word-wrap: break-word;
   font-family: "SF Pro Text", "Arial", sans-serif;
+  .loanOptions {
+    display: flex;
+    flex-direction: column;
+    span {
+      padding: 5px;
+    }
+  }
+  .option:hover {
+    color: black;
+    font-size: 16.3px;
+    cursor: pointer;
+  }
+  a {
+    text-decoration: none;
+    color: #2979ff;
+  }
 `;
 
 const InputContainer = styled.form`
@@ -69,50 +244,11 @@ const SendButton = styled.button`
   &:hover {
     background-color: #2962ff;
   }
+
+  &:disabled {
+    background-color: #ccc;
+    cursor: not-allowed;
+  }
 `;
-
-const Chatbot = () => {
-  const [messages, setMessages] = useState([]);
-
-  const botMessage = (messageText) => {
-    const initialMessageArray = ["Hello,", "Goodbye,", "Good,", "I want"];
-    if (initialMessageArray.find((str) => messageText.indexOf(str) > -1)) {
-      return { type: "bot", text: "Please, send your username" };
-    }
-  };
-
-  const handleMessageSubmit = (e) => {
-    e.preventDefault();
-    const messageText = e.target.message.value;
-    const newMessage = { type: "user", text: messageText };
-    setMessages([...messages, newMessage]);
-    botMessage(messageText);
-    e.target.message.value = "";
-  };
-
-  return (
-    <ChatbotContainer>
-      <MessagesContainer>
-        {messages.map((message, index) => (
-          <Message key={index} isUser={message.type === "user"}>
-            {message.type === "user" ? (
-              <UserMessage>{message.text}</UserMessage>
-            ) : (
-              <BotMessage>{message.text}</BotMessage>
-            )}
-          </Message>
-        ))}
-      </MessagesContainer>
-      <InputContainer onSubmit={handleMessageSubmit}>
-        <TextInput
-          type="text"
-          name="message"
-          placeholder="Digite uma mensagem..."
-        />
-        <SendButton type="submit">Enviar</SendButton>
-      </InputContainer>
-    </ChatbotContainer>
-  );
-};
 
 export default Chatbot;
