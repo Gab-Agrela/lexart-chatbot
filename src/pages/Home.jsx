56d15ae9529061c82974699587a836b18d2a0479
GabrielAgrela <gabriel.agrela@hotmail.com>
2023-07-18 16:20:16 -0300

    feat: css adjust

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,16 +15,25 @@ const Chatbot = () => {
   }, []);
 
   return (
-    <ChatbotContainer>
-      <MessagesContainer messages={messages} setMessages={setMessages} />
-      <FormContainer messages={messages} setMessages={setMessages} />
-    </ChatbotContainer>
+    <Container>
+      <ChatbotContainer>
+        <MessagesContainer messages={messages} setMessages={setMessages} />
+        <FormContainer messages={messages} setMessages={setMessages} />
+      </ChatbotContainer>
+    </Container>
   );
 };
 
+const Container = styled.div`
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
 const ChatbotContainer = styled.div`
-  width: 500px;
-  height: 600px;
+  width: 600px;
+  height: 98vh;
   background-color: #fff;
   border-radius: 8px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
